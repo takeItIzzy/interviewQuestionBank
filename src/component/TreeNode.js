06bd3081faf610277bd6124a08e73da8fbdef776
takeItIzzy <1367257497@qq.com>
2020-04-04 16:17:11 +0800

    第一次提交

--- src/component/TreeNode.js
+++ src/component/TreeNode.js
@@ -20,23 +20,17 @@ class TreeNode extends Component {
 		this.setState((prev, next) => ({
 			isShow: !this.state.isShow,
 		}));
-		console.log(this.props.store)
 	}
 
 	render() {
-		console.log(this.props.store)
 		return (
 			<div>
 				<p
-					className={this.state.aa ? "treeLineActive" : "treeLine"}
+					className="treeLine"
 					style={{
 						paddingLeft: this.state.level * 15 + "px",
 						display: "flex",
-						alignItems: "center",
-						// color:
-						// 	this.props.index && this.props.index === this.state.id
-						// 		? "#ced9e0"
-						// 		: "#e1e8ed",
+						alignItems: "center"
 					}}
 					onClick={() => this.leftClickTreeLine()}
 				>
@@ -64,7 +58,7 @@ class TreeNode extends Component {
 						this.state.child.map((i, k) => {
 							return (
 								<li key={i.id}>
-									<TreeNode {...i} fatherIndex={i.parentId} index={k}/>
+									<TreeNode {...i}/>
 								</li>
 							);
 						})}
